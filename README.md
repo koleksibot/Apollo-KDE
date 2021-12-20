8af904ac1c5ec1bf07b5f47d976c28ffdfc5a160
tahaluindo <94370774+tahaluindo@users.noreply.github.com>
2021-12-20 18:41:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,32 @@
+Apollo KDE - This is a beautiful dark theme for KDE Plasma desktop.
+
+In this repository you'll find:
+
+- Aurorae Theme
+- Konsole Color Schemes
+- Plasma Color Schemes
+- Plasma Desktop Theme
+- Plasma Look-and-Feel Settings
+- icons
+
+## Recommendations
+
+- For Blur enable translucency and blur effects on KDE sytemsettings. Set blur strength to six and noise to two and your blur effect will look great with the this theme.
+
+## Donate
+
+If you like my project, you can donate at:
+
+<span class="paypal"><a href="https://www.paypal.me/freefreeno" title="Donate to this project using Paypal"><img src="https://www.paypalobjects.com/webstatic/mktg/Logo/pp-logo-100px.png" alt="PayPal donate button" /></a></span>
+
+## Donate other ways without actually having to donate
+
+If you like my work and say you don't actually want to donate but you would like to support me in some way then the absolute best way to do that is download this theme from the location below and not from GitHub.
+
+https://www.pling.com/p/1317125
+
+and while your there have a look at my page and see what else you like. This would be greatly appreciated. Thanks
+
+## License
+
+GNU GPL v3
---
+++ plasma/look-and-feel/Apollo/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,145 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "709",
+                    "DialogWidth": "1066"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                },
+                "/General": {
+                    "showToolbox": "false",
+                    "sortMode": "-1"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///home/josh/Pictures/Apollo-flat.png"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "100"
+                        },
+                        "/Configuration/General": {
+                            "favoritesPortedToKAstats": "true",
+                            "systemApplications": "systemsettings.desktop,org.kde.kinfocenter.desktop"
+                        },
+                        "/Shortcuts": {
+                            "global": "Alt+F1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "0"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "26",
+                            "localPath": "/home/josh/.local/share/plasma_icons/org.kde.falkon.desktop",
+                            "url": "file:///usr/share/applications/org.kde.falkon.desktop"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icon"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "0"
+                        },
+                        "/Configuration/General": {
+                            "groupingAppIdBlacklist": "chromium.desktop",
+                            "groupingLauncherUrlBlacklist": "applications:chromium.desktop",
+                            "launchers": "applications:org.kde.konsole.desktop,applications:org.kde.dolphin.desktop"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.taskmanager"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "100"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "100"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "84",
+                    "DialogWidth": "1920"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                }
+            },
+            "height": 1.6666666666666667,
+            "hiding": "normal",
+            "location": "bottom",
+            "maximumLength": 106.66666666666667,
+            "minimumLength": 106.66666666666667,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
